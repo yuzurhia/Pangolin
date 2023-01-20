bff9201d10178e59293e8997624d6d145f3e5a1a
yuzurhia <phalyvong.tomy@gmail.com>
2023-01-20 17:42:14 +0100

    backEnd fonctionnalite

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormulaireComponent } from './formulaire/formulaire.component';
+import { PangolinComponent } from './pangolin/pangolin.component';
 
-const routes: Routes = [];
+const routes:Routes= [
+  {path: '/formulaire', component:FormulaireComponent},
+  {path: '/pangolin/:id' , component:PangolinComponent},
+  {path: '', redirectTo:'formulaire', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+
--- app/src/app/pangolin/pangolin.component.ts
+++ app/src/app/pangolin/pangolin.component.ts
@@ -1,18 +1,17 @@
 import { Component } from '@angular/core';
 import { OnInit } from '@angular/core';
 
-
 @Component({
   selector: 'app-pangolin',
   templateUrl: './pangolin.component.html',
-  styleUrls: ['./pangolin.component.css']
+  styleUrls: ['./pangolin.component.css'],
 })
 export class PangolinComponent {
-  nom:String="";
-  password: String="";
-  
-  OnInit(){
+  nom: String = '';
+  password: String = '';
+  role: String = '';
+
+  OnInit() {
     // initialiser les proprietes du composant
   }
-
 }
